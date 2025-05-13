ed4391278d112a051edbf2e45ea618e1738730b6
Petr Knap <8299754+petrknap@users.noreply.github.com>
2025-05-13 22:46:12 +0200

    refactor: request rate limiter as filter

--- src/main/java/cz/petrknap/website/Config.java
+++ src/main/java/cz/petrknap/website/Config.java
@@ -2,8 +2,10 @@ package cz.petrknap.website;
 
 import java.util.List;
 
-public record Config(BootGuard bootGuard, List<User> users) {
+public record Config(BootGuard bootGuard, RequestRateLimiter requestRateLimiter, List<User> users) {
     public record BootGuard(String sqliteCopyFile) {
+    }
+    public record RequestRateLimiter(int authorizationRequestsPerMinute) {
     }
     public record User(String username, String password, List<String> roles) {
     }
---
+++ src/main/java/cz/petrknap/website/RequestRateLimiter.java
@@ -0,0 +1,55 @@
+package cz.petrknap.website;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Semaphore;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class RequestRateLimiter extends OncePerRequestFilter {
+    private final Semaphore authorizationRequestSemaphore;
+    private final Timer timer = new Timer();
+
+    public RequestRateLimiter(Config config) {
+        authorizationRequestSemaphore = new Semaphore(config.requestRateLimiter().authorizationRequestsPerMinute());
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            if (request.getHeader("Authorization") != null) {
+                acquireForMinute(authorizationRequestSemaphore);
+            }
+            filterChain.doFilter(request, response);
+        } catch (LimitExceeded ignored) {
+            response.sendError(HttpStatus.TOO_MANY_REQUESTS.value());
+        }
+    }
+
+    private void acquireForMinute(Semaphore semaphore) throws LimitExceeded {
+        if (!semaphore.tryAcquire()) {
+            throw new LimitExceeded();
+        }
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                semaphore.release();
+            }
+        }, 60000);
+    }
+
+    private static class LimitExceeded extends Exception {
+    }
+}
